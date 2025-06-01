b1b5d698cf682aa4d0635ce6068ff872e430715e
Sankiii <sanketangane2006@gmail.com>
2025-06-01 12:58:15 +0530

    first commit

---
+++ lib/services/user_service.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:srch_prof/models/user_model.dart';
+
+class UserService {
+  final String baseUrl = "https://dummyjson.com/users";
+
+  
+}
